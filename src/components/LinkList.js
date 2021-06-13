9ae1545d233d9aa5a620855d239a2d990b4e33e8
Souptik Saha <iamthecubixnerd5@gmail.com>
2021-06-13 16:48:43 +0530

    Implemented Mutation and Updating the store

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,37 +1,46 @@
 import React from "react";
 import Link from "./Link";
-import {useQuery, gql} from "@apollo/client";
+import { useQuery, gql } from "@apollo/client";
 
-const FEED_QUERY = gql`
-    {
-        feed{
-            id
-            links{
-                id
-                createdAt
-                url
-                description
-            }
+export const FEED_QUERY = gql`
+  {
+    feed {
+      id
+      links {
+        id
+        createdAt
+        url
+        description
+        postedBy {
+          id
+          name
         }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
     }
+  }
 `;
 
 const LinkList = () => {
+  const { data } = useQuery(FEED_QUERY);
 
-        const {data} = useQuery(FEED_QUERY);
-
-        return(
-            <div>
-                {data && (
-                    <React.Fragment>
-                    {data.feed.links.map((link)=>(
-                        <Link key={link.id} link={link} />
-                    ))}
-                    </React.Fragment>
-                )}
-            </div>
-        );
-    };
+  return (
+    <div>
+      {data && (
+        <React.Fragment>
+          {data.feed.links.map((link, index) => (
+            <Link key={link.id} link={link} index={index} />
+          ))}
+        </React.Fragment>
+      )}
+    </div>
+  );
+};
 
 //   const linksToRender = [
 //     {
