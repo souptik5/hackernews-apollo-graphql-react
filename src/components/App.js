9ce42832704dc06323ce163e5bdb2440a5031a11
Souptik Saha <iamthecubixnerd5@gmail.com>
2021-06-17 21:09:48 +0530

    Implemented Search feature

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,22 @@ import CreateLink from "./CreateLink";
 import Header from "./Header";
 import LinkList from "./LinkList";
 import Login from "./Login";
-import { Switch, Route } from "react-router-dom";
+import Search from "./Search";
+import { Switch, Route, Redirect } from "react-router-dom";
 
-const App = () => {
-  return (
-    <div className="center w85">
-      <Header />
-      <div className="ph3 pv1 background-gray">
-        <Switch>
-          <Route exact path="/" component={LinkList} />
-          <Route exact path="/create" component={CreateLink} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </div>
+const App = () => (
+  <div className="center w85">
+    <Header />
+    <div className="ph3 pv1 background-gray">
+      <Switch>
+        <Route exact path="/" component={LinkList} render={() => <Redirect to="/new/1" />} />
+
+        <Route exact path="/create" component={CreateLink} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/search" component={Search} />
+      </Switch>
     </div>
-  );
-};
+  </div>
+);
 
 export default App;
